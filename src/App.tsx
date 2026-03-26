c689a6111b718c96fc0b4f239aea7f161fe1cccc
innaaznauryan <inna.aznauryan@gmail.com>
2026-03-26 17:24:20 +0400

    scroll to top on route change

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "@/components/layout/Layout";
+import ScrollToTop from "@/components/ScrollToTop.tsx";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Collections from "./pages/Collections";
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Layout>
           <Routes>
             <Route path="/" element={<Index />} />
